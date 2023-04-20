7c04ce3c89c5d827fa00cb6e1e4b20979d4a60d8
David <david_emmi@hotmail.com>
2023-04-19 23:31:57 -0600

    Creación de repo para vistas de proyecto
    Se crea el repositorio para incluir el proyecto en angular y dentro de este ir implementando las vista necesarias para la entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { SearchComponent } from './pages/search/search.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    AuthComponent,
+    SearchComponent,
+    DetailsComponent,
+    CheckoutComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
