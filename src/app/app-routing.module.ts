c84c2091d9bfc17ec03491d6e1b504b9deeb6d2c
David <david_emmi@hotmail.com>
2023-04-23 16:38:30 -0600

    Página para buscar terminada
    Se terminó el diseño de la página de búsqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'auth', component:AuthComponent},
-  {path: 'detalles', component:DetailsComponent}
+  {path: 'detalles', component:DetailsComponent},
+  {path: 'search',component:SearchComponent}
 ];
 
 @NgModule({
