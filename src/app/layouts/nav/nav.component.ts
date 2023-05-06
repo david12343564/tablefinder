e7976808dc38960f0d1dd1da9f92b3e7dbe044c8
Lilia Lobato <ie706937@iteso.mx>
2023-05-05 21:26:44 -0600

    loggin

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { TokenService } from 'src/app/services/token.service'; 
+import { LoginService } from 'src/app/services/login.service'; 
+
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  
+  constructor(
+    private tokenService: TokenService,
+    public router: Router,
+    private loginService: LoginService
+  ) {
+    this.tokenService.authStatus.subscribe((status: boolean) => {
+      this.logueado = status;
+    })
+  }
+
+  logueado: boolean = false;
+
+  cerrarSesion() {
+    this.tokenService.deleteToken();
+    this.router.navigate(['/auth']);
+  }
 
 }
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms'
+import { Router } from '@angular/router';
+
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { Credenciales } from 'src/app/shared/interfaces/credenciales';
+import { LoginService } from 'src/app/services/login.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +12,28 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
   formLogin: FormGroup
 
-  constructor(formBuilder: FormBuilder){
+  constructor(formBuilder: FormBuilder, private loginService: LoginService,
+    private tokenService: TokenService, private router: Router){
     this.formLogin = formBuilder.group({
-      email: ['', Validators.required,Validators.email],
+      email: ['', Validators.required],
       password: ['',Validators.required]
     })
+  }
+
+  
+  credenciales:  Credenciales = { email: '', password: '' };
+
+  iniciarSesion() {
+    console.log(this.credenciales)
+    this.loginService.login(this.credenciales).subscribe((data: any) => {
+      // Recibimos el token
+      this.tokenService.setToken(data.token);
+      // Enviar a tareas
+      this.router.navigate(['/']);
+    });
   }
 
 }
